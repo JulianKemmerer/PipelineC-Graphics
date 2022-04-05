02b959a7f1a6013afb7bcedecc6ee837c63bb93a
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-04-05 14:34:26 -0400

    Sketch of what top glue will look like once vhdl is genrated

--- top_glue_no_struct.vhd
+++ top_glue_no_struct.vhd
@@ -2,37 +2,57 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.c_structs_pkg.all;
+use work.clk_cross_t_pkg.all;
 
 entity top_glue_no_struct is
+generic(
+    pixel_logic_latency : integer := 0;
+);
 port(
     videoclk : in std_logic;
-    render_pixel_interactive_x : in unsigned(15 downto 0);
-    render_pixel_interactive_y : in unsigned(15 downto 0);
-    render_pixel_interactive_reset : in unsigned(0 downto 0);
-    render_pixel_interactive_vsync : in unsigned(0 downto 0);
-    render_pixel_interactive_button : in unsigned(0 downto 0);
-    render_pixel_interactive_return_output_a : out unsigned(7 downto 0);
-    render_pixel_interactive_return_output_b : out unsigned(7 downto 0);
-    render_pixel_interactive_return_output_g : out unsigned(7 downto 0);
-    render_pixel_interactive_return_output_r : out unsigned(7 downto 0)
+    video_x : in unsigned(15 downto 0);
+    video_y : in unsigned(15 downto 0);
+    jump_pressed : in unsigned(0 downto 0);
+    pixel_a : out unsigned(7 downto 0);
+    pixel_r : out unsigned(7 downto 0);
+    pixel_g : out unsigned(7 downto 0);
+    pixel_b : out unsigned(7 downto 0)
 );
 end top_glue_no_struct;
 
 architecture arch of top_glue_no_struct is 
+
+-- Clock cross wires
+signal frame_clock : std_logic;
+signal state : full_state_t;
+signal clk_cross_to_module : clk_cross_to_module_t;
+
 begin
 
-top_inst : entity work.top
+-- VHDL fix to match generated hashes in code
+clk_cross_to_module.pixel_logic.render_pixel_pipelinec_app_c_l94_c20_3fa4.get_scene_tr_pipelinec_gen_c_l377_c19_18d6.state_wire_READ_return_output <= state;
+
+pixel_logic_inst : entity work.pixel_logic_NCLK_HASH_TODO
 port map(
-    clk_25p0 => videoclk,
-    render_pixel_interactive_x => render_pixel_interactive_x,
-    render_pixel_interactive_y => render_pixel_interactive_y,
-    render_pixel_interactive_reset => render_pixel_interactive_reset,
-    render_pixel_interactive_vsync => render_pixel_interactive_vsync,
-    render_pixel_interactive_button => render_pixel_interactive_button,
-    render_pixel_interactive_return_output.a => render_pixel_interactive_return_output_a,
-    render_pixel_interactive_return_output.b => render_pixel_interactive_return_output_b,
-    render_pixel_interactive_return_output.g => render_pixel_interactive_return_output_g,
-    render_pixel_interactive_return_output.r => render_pixel_interactive_return_output_r
+    clk => videoclk,
+    CLOCK_ENABLE => to_unsigned(1,1),
+    clk_cross_to_module => clk_cross_to_module.pixel_logic,
+    x => video_x,
+    y => video_y,
+    latency => to_unsigned(pixel_logic_latency, 16),
+    return_output.color.a => pixel_a,
+    return_output.color.r => pixel_r,
+    return_output.color.g => pixel_g,
+    return_output.color.b => pixel_b,
+    return_output.frame_clock(0) => frame_clock
+);
+
+frame_logic_inst : entity work.frame_logic_0CLK_HASH_TODO
+port map(
+    clk => frame_clock,
+    CLOCK_ENABLE => to_unsigned(1,1),
+    jump_pressed => jump_pressed,
+    return_output => state
 );
 
 end arch;
