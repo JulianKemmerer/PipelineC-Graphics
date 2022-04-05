1ed86823961e23f4d0196905583ca2095c3fb939
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-04-05 14:53:18 -0400

    Fix top glue to correctly use top with minimal extra wiring for litex

--- top_glue_no_struct.vhd
+++ top_glue_no_struct.vhd
@@ -2,7 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.c_structs_pkg.all;
-use work.clk_cross_t_pkg.all;
 
 entity top_glue_no_struct is
 generic(
@@ -22,37 +21,28 @@ end top_glue_no_struct;
 
 architecture arch of top_glue_no_struct is 
 
--- Clock cross wires
+-- Special wrapper wiring for LITEX demo 
+-- that doesnt include VGA timing internally
 signal frame_clock : std_logic;
 signal state : full_state_t;
-signal clk_cross_to_module : clk_cross_to_module_t;
 
 begin
 
--- VHDL fix to match generated hashes in code
-clk_cross_to_module.pixel_logic.render_pixel_pipelinec_app_c_l94_c20_3fa4.get_scene_tr_pipelinec_gen_c_l377_c19_18d6.state_wire_READ_return_output <= state;
-
-pixel_logic_inst : entity work.pixel_logic_NCLK_HASH_TODO
+top_inst : entity work.top 
 port map(
-    clk => videoclk,
-    CLOCK_ENABLE => to_unsigned(1,1),
-    clk_cross_to_module => clk_cross_to_module.pixel_logic,
-    x => video_x,
-    y => video_y,
-    latency => to_unsigned(pixel_logic_latency, 16),
-    return_output.color.a => pixel_a,
-    return_output.color.r => pixel_r,
-    return_output.color.g => pixel_g,
-    return_output.color.b => pixel_b,
-    return_output.frame_clock(0) => frame_clock
-);
-
-frame_logic_inst : entity work.frame_logic_0CLK_HASH_TODO
-port map(
-    clk => frame_clock,
-    CLOCK_ENABLE => to_unsigned(1,1),
-    jump_pressed => jump_pressed,
-    return_output => state
+    clk_25p0 => videoclk,
+    clk_60p0hz => frame_clock,
+    pixel_logic_x => video_x,
+    pixel_logic_y => video_y,
+    pixel_logic_latency => to_unsigned(pixel_logic_latency, 16),
+    pixel_logic_return_output.color.a => pixel_a,
+    pixel_logic_return_output.color.r => pixel_r,
+    pixel_logic_return_output.color.g => pixel_g,
+    pixel_logic_return_output.color.b => pixel_b,
+    pixel_logic_return_output.frame_clock(0) => frame_clock,
+    frame_logic_jump_pressed => jump_pressed,
+    frame_logic_return_output => state,
+    state_wire_fake_fake_input => state
 );
 
 end arch;
