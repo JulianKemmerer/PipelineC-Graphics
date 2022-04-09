56e8ea632cee853553b65e8c0966775e80029f4d
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-04-09 17:23:52 -0400

    Changes for full RT verilator, litex macro not set

--- pipelinec_app.c
+++ pipelinec_app.c
@@ -1,9 +1,9 @@
 #define CCOMPILE
 #ifdef __PIPELINEC__
 //#pragma PART "LFE5U-85F-6BG381C" // An ECP5U 85F part
-#pragma PART "xc7a35ticsg324-1l" // Arty 35t
-//#pragma PART "xc7a100tcsg324-1" // Arty 100t
-#define LITEX_INTEGRATION //TEMP
+//#pragma PART "xc7a35ticsg324-1l" // Arty 35t
+#pragma PART "xc7a100tcsg324-1" // Arty 100t
+
 #include "pipelinec_app_vgaconfig.h"
 
 #include "compiler.h"
@@ -37,14 +37,15 @@
 // Scene helper func declared before use in funcs below but after the scene_t struct definition
 #include "get_scene.h" */
 
-// LITEX versions of functions, can integrate with NOT-LITEX versions later
-#ifdef LITEX_INTEGRATION
 // Define the user created frame clock
 #define FRAME_CLK_MHZ 6e-5 // 60Hz
 uint1_t frame_clock;
 #include "clock_crossing/frame_clock.h"
 #pragma ASYNC_WIRE frame_clock
 CLK_MHZ(frame_clock, FRAME_CLK_MHZ)
+
+// LITEX versions of functions, can integrate with NOT-LITEX versions later
+#ifdef LITEX_INTEGRATION
 
 // Helper func make toggling clock with isolated static frame_clock_reg
 // Litex version does not include vga active signal
@@ -151,7 +152,6 @@ inline user_input_t get_user_input()
   user_input_t i;
   // For now only exists in hardware
   #ifdef __PIPELINEC__
-  #ifndef LITEX_INTEGRATION
   // Read buttons wire/board IO port
   uint4_t btns;
   WIRE_READ(uint4_t, btns, buttons) // btns = buttons
@@ -160,7 +160,6 @@ inline user_input_t get_user_input()
   // Select which buttons and switches do what?
   i.jump_pressed = btns >> 0;
   i.reset_pressed = btns >> 3;
-  #endif
   #else
   // TODO user IO for running as C code
   i.jump_pressed = 1;
@@ -193,21 +192,13 @@ void frame_logic()
 {
   static full_state_t state; // The state registers
   static uint1_t power_on_reset = 1;
-  static uint1_t reset_pressed;
-  static uint1_t jump_pressed;
 
   // Drive scene from register
   WIRE_WRITE(full_state_t, state_wire, state)
 
-#ifndef LITEX_INTEGRATION
   // Read user input
   user_input_t ui = get_user_input();
-  reset_pressed = ui.reset_pressed; 
-  jump_pressed = ui.jump_pressed; 
-#else
-  reset_pressed = 0;
-  jump_pressed = 0;
-#endif
+
   // Normal next state update
   state = full_update(state, ui.reset_pressed | power_on_reset, ui.jump_pressed);
   power_on_reset = 0;
