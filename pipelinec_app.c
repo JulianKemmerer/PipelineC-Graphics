0e997d1b22550860e4d43676c0fdf6c81e355454
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-06-30 18:31:49 -0400

    Added uart module that works with buttons

--- pipelinec_app.c
+++ pipelinec_app.c
@@ -41,6 +41,84 @@
 #include "fixed_type.h"
 #include "tr_pipelinec.gen.c"
 
+// Define the user created frame clock
+#define FRAME_CLK_MHZ 6e-5 // 60Hz
+uint1_t frame_clock;
+CLK_MHZ(frame_clock, FRAME_CLK_MHZ)
+uint1_t frame_clock_rising_edge;
+uint1_t frame_clock_falling_edge;
+
+// Helper func to drive frame clock from isolated static frame_clock_reg
+// Running on pixel clock, maybe include inside vga_timing?
+void frame_clock_logic(uint16_t x, uint16_t y, bool active)
+{
+  // Need to make ~50% duty cycle frame clock
+  static uint1_t frame_clock_reg;
+  // Drive clock from register
+  frame_clock = frame_clock_reg;
+  frame_clock_rising_edge = 0;
+  frame_clock_falling_edge = 0;
+
+  // Falling edge mid frame (does not update state)
+  if( active & 
+     (x==(FRAME_WIDTH/2)) &
+     (y==(FRAME_HEIGHT/2))
+  ){
+    frame_clock_reg = 0;
+    frame_clock_falling_edge = 1;
+  }
+  // Rising edge at end of frame (state updated during back porch)
+  else if(active & 
+         (x==(FRAME_WIDTH-1)) &
+         (y==(FRAME_HEIGHT-1))
+  ){
+    frame_clock_reg = 1;
+    frame_clock_rising_edge = 1;
+  }
+}
+
+// UART module that augments buttons behavior
+// Run uart at pixel clock to avoid multiple clocks
+#define UART_CLK_MHZ PIXEL_CLK_MHZ
+#include "uart/uart_mac.c"
+// Receive bytes over uart 
+// continuosuly updating rx buffer
+uint4_t uart_buttons;
+MAIN_MHZ(uart_buttons_rx, UART_CLK_MHZ)
+void uart_buttons_rx()
+{
+  // Always ready for incoming uart bytes
+  uart_rx_mac_out_ready = 1;
+  // Overwrite buffer as they arrive
+  if(uart_rx_mac_word_out.valid)
+  {
+    uart_buttons = uart_rx_mac_word_out.data;
+  }
+}
+// Capture stable button value in time for frame clock rising edge
+// Game logic uses OR of buttons or uart data
+uint4_t buttons_or_uart;
+#pragma ASYNC_WIRE buttons_or_uart // Read by slow frame clock domain
+// Transmit out the button value used to update the state
+MAIN_MHZ(uart_buttons_tx, UART_CLK_MHZ)
+void uart_buttons_tx()
+{
+  // Upon rising edge of frame clock, 
+  // transmit the buttons_or_uart state that was stable and used by full_update
+  uart_tx_mac_word_in.valid = frame_clock_rising_edge;
+  uart_tx_mac_word_in.data = buttons_or_uart;
+  // TODO if !uart_tx_mac_in_ready then overflow/error
+  
+  // Upon falling edge of frame clock sample the button state
+  // the game will use to update next state at the rising edge
+  if(frame_clock_falling_edge)
+  {
+    // OR of current buttons or current and uart state
+    // is what game logic samples for use
+    buttons_or_uart = buttons | uart_buttons;
+  }  
+}
+
 // User input
 typedef struct user_input_t
 {
@@ -52,12 +130,9 @@ inline user_input_t get_user_input()
   user_input_t i;
   // For now only exists in hardware
   #ifdef __PIPELINEC__
-  // Read buttons wire/board IO port
-  uint4_t btns = buttons;
-  //uint4_t sws = switches;
   // Select which buttons and switches do what?
-  i.jump_pressed = btns >> 0;
-  i.reset_pressed = btns >> 3;
+  i.jump_pressed = buttons_or_uart >> 0;
+  i.reset_pressed = buttons_or_uart >> 3;
   #else
   // TODO user IO for running as C code
   i.jump_pressed = 1;
@@ -66,35 +141,6 @@ inline user_input_t get_user_input()
   return i;
 }
 
-// Define the user created frame clock
-#define FRAME_CLK_MHZ 6e-5 // 60Hz
-uint1_t frame_clock;
-CLK_MHZ(frame_clock, FRAME_CLK_MHZ)
-
-// Helper func to drive frame clock from isolated static frame_clock_reg
-// Running on pixel clock, maybe include inside vga_timing?
-void frame_clock_logic(uint16_t x, uint16_t y, bool active)
-{
-  // Need to make ~50% duty cycle frame clock
-  static uint1_t frame_clock_reg;
-  // Drive clock from register
-  frame_clock = frame_clock_reg;
-
-  // Falling edge mid frame (does not update state)
-  if( active & 
-     (x==(FRAME_WIDTH/2)) &
-     (y==(FRAME_HEIGHT/2))
-  ){
-    frame_clock_reg = 0;
-  }
-  // Rising edge at end of frame (state updated during back porch)
-  else if(active & 
-         (x==(FRAME_WIDTH-1)) &
-         (y==(FRAME_HEIGHT-1))
-  ){
-    frame_clock_reg = 1;
-  }
-}
 
 // Per frame next state comb. logic runnning on frame clock
 full_state_t state; // The state register, shared global register
@@ -140,3 +186,4 @@ void pixel_logic()
   // Drive output signals/registers
   pmod_register_outputs(vga_signals, color);
 }
+
