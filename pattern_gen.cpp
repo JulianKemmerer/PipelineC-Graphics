eea8a9ff20138df873e97e49e114dbee79956ef8
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-10-25 11:59:12 -0300

    made pattern generator frame dependent and button dependent

--- pattern_gen.cpp
+++ pattern_gen.cpp
@@ -9,15 +9,20 @@ full_state_t state;
 
 full_state_t full_update(INOUT(full_state_t) state, bool reset, bool button_state)
 {
+  state.scene.frame = state.scene.frame + 1;
+  state.won = button_state;
   return state;
 }
 
 inline pixel_t render_pixel(uint16_t i, uint16_t j)
 {
+  uint16_t frame = state.scene.frame;
+  bool button = state.won;
+  
   pixel_t pix;
-  pix.r = i + j;
-  pix.g = i ^ j;
-  pix.b = 128;
+  pix.r = button ? i + j : i - j;
+  pix.g = i ^ (j + frame);
+  pix.b = frame;
   return pix;
 }
 
