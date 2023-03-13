497102e70ff1666d93b2ff3f8afa0a1d2b7506aa
Victor Suarez Rovere <suarezvictor@gmail.com>
2023-03-12 23:19:51 -0300

    overloaded float operators

--- float_type.h
+++ float_type.h
@@ -14,9 +14,27 @@ inline float float_shift(float x, int shift) { return shift > 0 ? x * (1<<shift)
 //correct float_shift implementation in hardware just adds the shift to exponent
 //inline float float_shift(float x, int shift) { fp_tlayout conv; conv.f = x; conv.exp+=shift; return conv.f; }
 
+//main overloaded float operators
+#define float_mul(a,b) ((a)*(b))
+#define float_add(a,b) ((a)+(b))
+#define float_sub(a,b) ((a)-(b))
+#define float_div(a,b) ((a)/(b))
 
 #endif
 #endif
+
+//misc overloaded float operators
+#define float_lt(a, b) ((a)<(b))
+#define float_gt(a, b) ((a)>(b))
+#define float_eq(a, b) ((a)==(b))
+#define float_neq(a, b) ((a)!=(b))
+
+#define float_mul_double(a, b) float_mul(a, b)
+#define float_sub_double(a, b) float_sub(a, b)
+#define float_neq_double(a, b) float_neq(a, b)
+#define float_lt_double(a, b) float_lt(a, b)
+#define float_gt_double(a, b) float_gt(a, b)
+#define double_div_float(a, b) float_div(a, b)
 
 #include "custom_float.h" //may redefine float
 
