52febf12a73995c5b44383128596b388a2c12fe0
Victor Suarez Rovere <suarezvictor@gmail.com>
2023-03-12 23:35:19 -0300

    add default implementation for overloaded float operators for pipelinec

---
+++ pipelinec_float_type.h
@@ -0,0 +1,17 @@
+#ifndef __PIPELINEC_FLOAT_TYPE_H__
+#define __PIPELINEC_FLOAT_TYPE_H__
+
+#ifndef float_mul
+float float_mul(float a, float b) { return a*b; }
+#endif
+#ifndef float_add
+float float_add(float a, float b) { return a+b; }
+#endif
+#ifndef float_sub
+float float_sub(float a, float b) { return a-b; }
+#endif
+#ifndef float_div
+float float_div(float a, float b) { return a/b; }
+#endif
+
+#endif //__PIPELINEC_FLOAT_TYPE_H__
